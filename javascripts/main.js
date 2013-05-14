ad396954220cc048ed5ad609ba7ab0420500592c
Kévin Pausé <kevin.pause@supinfo.com>
2013-05-14 16:57:50 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
   $('input[type=button]').click(function() {
     $('#rotate').rotate(45,1000);  
   });
+  
+  $('input[type=submit]').click(function() {
+    var html = $('#code textarea').html();
+    
+    $('#result').html(html);
+  });
 });
